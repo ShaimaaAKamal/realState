da806f4ddec39d03fa7929f1c3bed8844c1b4d30
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2023-01-29 20:37:29 +0200

    add app basic routing

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,29 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from '../Home/Home';
+import Layout from '../Layout/Layout';
+import Blog from '../Blog/Blog';
+import About from '../About/About';
+import Services from '../Services/Services';
+import Pricing from '../Pricing/Pricing';
+import Contact from '../Contact/Contact';
+import NotFound from '../NotFound/NotFound';
 import './App.scss';
 
 function App() {
+  const routes=createBrowserRouter([
+        {path:'',element:<Layout/>,errorElement:<NotFound/>,children:[
+          {index:true,element:<Home/>},
+           {path:'aboy',element:<About/>},
+          {path:'blog',element:<Blog/>},
+          {path:'services',element:<Services/>},
+          {path:'pricing',element:<Pricing/>},
+          {path:'contact',element:<Contact/>},
+        ]}
+  ])
   return (
     <div className="App">
+       <RouterProvider router={routes}>
+       </RouterProvider>
     </div>
   );
 }
