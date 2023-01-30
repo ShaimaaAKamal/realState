f2231211a1338645fcb617c586620dfe52892149
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2023-01-30 08:25:21 +0200

    handle navbar displayimg accross different routes

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../Shared/Navbar/Navbar';
 import Footer from '../Shared/Footer/Footer';
 
 export default function Layout() {
+  const location=useLocation();
+  const path=location.pathname;
+    useEffect(() => {
+    const open=document.querySelector('.open');
+    const close=document.querySelector('.close');
+    const collapse=document.querySelector('.collapse')
+   if(collapse.classList.contains('show')){
+    open.classList.remove('d-none');
+    close.classList.add('d-none');
+    collapse.classList.remove('show');
+   } 
+  },[path]);
   return (
    <>
       <Navbar/>
